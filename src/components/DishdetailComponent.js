72b36a6c27dabdf4f3077e620bb81364316b8605
vupro123kute@gmail.com <vupro123kute@gmail.com>
2022-07-12 14:42:01 +0700

    Functional Components

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+// class Dishdetail extends Component {
+//   // renderDish(dish) {
+//   //   return (
+//   //     <div className="col-12 col-md-5 m-4 border rounded">
+//   //       <Card>
+//   //         <CardImg top src={dish.image} alt={dish.name} />
+//   //         <CardBody>
+//   //           <CardTitle>{dish.name}</CardTitle>
+//   //           <CardText>{dish.description}</CardText>
+//   //         </CardBody>
+//   //       </Card>
+//   //     </div>
+//   //   );
+//   // }
+
+//   // renderComments(comments) {
+//   //   if (comments != null) {
+//   //     return (
+//   //       <div className="col-12 col-md-5 m-4 border rounded">
+//   //         <h2>Comments</h2>
+//   //         <ul className="list-unstyled">
+//   //           {comments.map((comment) => {
+//   //             // Custom
+//   //             if (comment.id == this.props.dish.id) {
+//   //               return (
+//   //                 <li key={comment.id}>
+//   //                   <p>{comment.comment}</p>
+//   //                   <p>
+//   //                     -- {comment.author} ,{" "}
+//   //                     {new Intl.DateTimeFormat("en-US", {
+//   //                       year: "numeric",
+//   //                       month: "short",
+//   //                       day: "2-digit",
+//   //                     }).format(new Date(Date.parse(comment.date)))}
+//   //                   </p>
+//   //                 </li>
+//   //               );
+//   //             }
+//   //           })}
+//   //         </ul>
+//   //       </div>
+//   //     );
+//   //   } else {
+//   //     return <div></div>;
+//   //   }
+//   // }
+
+//   render() {
+//     if (this.props.dish != null) {
+//       return (
+//         <div className="container">
+//           <div className="row justify-content-center">
+//             {this.renderDish(this.props.dish)}
+//             {this.renderComments(this.props.dish.comments)}
+//           </div>
+//         </div>
+//       );
+//     } else {
+//       return <div></div>;
+//     }
+//   }
+// }
+
+// ======================== Functional Component ========================
+
+function RenderDish( dish ) {
+  return (
+    <div className="col-12 col-md-5 m-4 border rounded">
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+function RenderComments( comments ) {
+  if (comments != null) {
+    return (
+      <div className="col-12 col-md-5 m-4 border rounded">
+        <h2>Comments</h2>
+        <ul className="list-unstyled">
+          {comments.map((comment) => {
+            // Custom
+            if (comment.id == this.props.dish.id) {
+              return (
+                <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>
+                    -- {comment.author} ,{" "}
+                    {new Intl.DateTimeFormat("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    }).format(new Date(Date.parse(comment.date)))}
+                  </p>
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+}
+
+const Dishdetail = (props) => {
+  if (this.props.dish != null) {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          <RenderDish dish={this.props.dish}/>
+          <RenderComments comments={this.props.dish.comments} />
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+}
+
+export default Dishdetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -9,58 +9,91 @@ import {
   CardTitle,
 } from "reactstrap";
 
-class Menu extends Component {
-  constructor(props) {
-    super(props);
+// class Menu extends Component {
+//   //  Move to DishDetailComponent ---------------------------------
+//   // constructor(props) {
+//   //   super(props);
+//   //   this.state = {
+//   //     selectedDish: null,
+//   //   };
+//   // }
 
-    this.state = {
-      selectedDish: null,
-    };
-  }
+//   // onDishSelect(dish) {
+//   //   this.setState({ selectedDish: dish });
+//   // }
 
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish });
-  }
+//   //   renderDish(dish) {
+//   //     if (dish != null)
+//   //       return (
+//   //         <Card>
+//   //           <CardImg top src={dish.image} alt={dish.name} />
+//   //           <CardBody>
+//   //             <CardTitle>{dish.name}</CardTitle>
+//   //             <CardText>{dish.description}</CardText>
+//   //           </CardBody>
+//   //         </Card>
+//   //       );
+//   //     else return <div></div>;
+//   //   }
+//   //  Move to DishDetailComponent =================================
 
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else return <div></div>;
-  }
+//   render() {
+//     const menu = this.props.dishes.map((dish) => {
+//       return (
+//         <div className="col-12 col-md-5 m-4 border rounded">
+//           {/* <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>      =>  props onClick() at MainComponent*/}
+//           <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
+//             <CardImg width="100%" src={dish.image} alt={dish.name} />
+//             <CardImgOverlay>
+//               <CardTitle>{dish.name}</CardTitle>
+//             </CardImgOverlay>
+//           </Card>
+//         </div>
+//       );
+//     });
 
-  render() {
-    const menu = this.props.dishes.map((dish) => {
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-              <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
-    });
+//     return (
+//       <div className="container">
+//         <div className="row justify-content-center">{menu}</div>
+//         {/* <div className="row">
+//           <div className="col-12 col-md-5 m-1">
+//             {this.renderDish(this.state.selectedDish)}
+//           </div>
+//         </div> */}
+//       </div>
+//     );
+//   }
+// }
 
+// ======================== Functional Component ========================
+
+function RenderMenuItem({ dish, onClick }) {
+  return (
+    <div className="col-12 col-md-5 m-4 border rounded">
+      <Card onClick={() => onClick(dish.id)}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    </div>
+  );
+}
+
+const Menu = (props) => {
+  const menu = props.dishes.map((dish) => {
     return (
-      <div className="container">
-        <div className="row">{menu}</div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-        </div>
+      <div className="col-12 col-md-5 m-1" key={dish.id}>
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">{menu}</div>
+    </div>
+  );
+};
 
 export default Menu;
